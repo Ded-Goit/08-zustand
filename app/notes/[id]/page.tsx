68839a66afb585c71fb9de707d1766d2351fc5cf
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-06 15:46:49 +0300

    added old cod

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -6,6 +6,37 @@ import {
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 
+type NoteDetailsProps = {
+  params: Promise<{ id: string }>;
+};
+
+async function NoteDetails({ params }: NoteDetailsProps) {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+  const parseId = Number(id);
+  queryClient.prefetchQuery({
+    queryKey: ["note", parseId],
+    queryFn: () => fetchNoteById(parseId),
+  });
+
+  return (
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NoteDetailsClient />
+      </HydrationBoundary>
+    </>
+  );
+}
+
+export default NoteDetails;
+/**import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const note = await fetchNoteById(Number(params.id));
 
@@ -43,4 +74,4 @@ export default async function NoteDetails({
       <NoteDetailsClient />
     </HydrationBoundary>
   );
-}
+}*/
