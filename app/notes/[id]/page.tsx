7e216952c22a9e0d9d0895a54744b88ff6f45987
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-06 16:44:49 +0300

    edit bug

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,11 +5,37 @@ import {
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+import { Metadata } from "next";
 
 type NoteDetailsProps = {
   params: Promise<{ id: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: NoteDetailsProps): Promise<Metadata> {
+  const { id } = await params;
+  const parsedId = Number(id);
+  const note = await fetchNoteById(parsedId);
+
+  return {
+    title: note.title,
+    description: note.content?.slice(0, 100),
+    openGraph: {
+      title: note.title,
+      description: note.content?.slice(0, 100),
+      url: `https://08-zustand-puce.vercel.app/notes/${id}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Note Details",
+        },
+      ],
+    },
+  };
+}
 async function NoteDetails({ params }: NoteDetailsProps) {
   const { id } = await params;
   const queryClient = new QueryClient();
@@ -29,49 +55,3 @@ async function NoteDetails({ params }: NoteDetailsProps) {
 }
 
 export default NoteDetails;
-/**import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from "@tanstack/react-query";
-import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const note = await fetchNoteById(Number(params.id));
-
-  return {
-    title: `Note: ${note.title} | NoteHub`,
-    description: note.content?.slice(0, 150) || "",
-    openGraph: {
-      title: `Note: ${note.title} | NoteHub`,
-      description: note.content?.slice(0, 150) || "",
-      url: `https://08-zustand-puce.vercel.app/notes/${params.id}`,
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        },
-      ],
-    },
-  };
-}
-
-export default async function NoteDetails({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const parseId = Number(params.id);
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["note", parseId],
-    queryFn: () => fetchNoteById(parseId),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient />
-    </HydrationBoundary>
-  );
-}*/
